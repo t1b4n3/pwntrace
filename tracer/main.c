c4345ca014c923bc6285ac03e33aa84a13a6bade
tibane0 <nkatekotibane101@gmail.com>
2025-11-10 22:04:44 +0200

    update

---
+++ tracer/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+
+
+// run pid
+void parse_args(char**argv) {
+
+}
+
+int main(int argc, char* argv[]) {
+	
+}
